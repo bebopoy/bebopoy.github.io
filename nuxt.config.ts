9e0ab1e1eedeb0c061a43e6b7a1b753d111bfa0f
bebopoy <xiangtongnie@gmail.com>
2025-07-08 00:31:11 +0800

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,63 +1,79 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { siteConfig } from './site.config'
+import { siteConfig } from "./site.config";
+import { defineNuxtConfig } from "nuxt/config";
+import { presetUno, presetIcons } from "unocss";
 
 export default defineNuxtConfig({
   modules: [
-    '@unocss/nuxt',
-    '@vueuse/nuxt',
-    '@nuxt/content',
-    '@nuxtjs/stylelint-module',
+    "@unocss/nuxt",
+    "@vueuse/nuxt",
+    "@nuxt/content",
+    "@nuxtjs/stylelint-module",
   ],
+  unocss: {
+    presets: [
+      presetUno(),
+      presetIcons({
+        scale: 1.2,
+        extraProperties: {
+          display: "inline-block",
+          "vertical-align": "middle",
+        },
+      }),
+    ],
+  },
   app: {
-    rootId: 'nuxt-root',
+    rootId: "Hutiger-s-blog",
     head: {
       meta: [
-        { name: 'description', content: siteConfig.description },
-        { name: 'author', content: siteConfig.author },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
-        { name: 'revisit-after', content: '7 days' },
-        { name: 'msapplication-TileColor', content: '#ffffff' },
-        { charset: 'UTF-8' },
-        { 'http-equiv': 'X-UA-Compatible', 'content': 'IE=edge' },
+        { name: "description", content: siteConfig.description },
+        { name: "author", content: siteConfig.author },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+        { name: "revisit-after", content: "7 days" },
+        { name: "msapplication-TileColor", content: "#ffffff" },
+        { charset: "UTF-8" },
+        { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
       ],
-      noscript: [
-        { children: 'JavaScript is required' },
+      noscript: [{ children: "JavaScript is required" }],
+      script: [
+        {
+          src: "https://cn.vercount.one/js",
+          defer: true,
+        },
       ],
       htmlAttrs: {
         lang: siteConfig.lang,
       },
       bodyAttrs: {
-        class: 'font-sans',
+        class: "font-sans",
       },
     },
+  },
+  nitro: {
+    preset: "static",
   },
   content: {
     highlight: {
       theme: {
         // Default theme (same as single string)
-        default: 'vitesse-light',
+        default: "vitesse-light",
         // Theme used if `html.dark`
-        dark: 'vitesse-dark',
+        dark: "vitesse-dark",
         // Theme used if `html.sepia`
-        sepia: 'monokai',
+        sepia: "monokai",
       },
-      preload: [
-        'c',
-        'cpp',
-        'java',
-      ],
-
+      preload: ["c", "cpp", "java"],
     },
   },
   css: [
-    '@unocss/reset/tailwind.css',
-    '@/assets/styles/global.scss',
-    '@/assets/styles/theme.css',
-    '@/assets/styles/transition.css',
-    '@/assets/styles/markdown.scss',
+    "@unocss/reset/tailwind.css",
+    "@/assets/styles/global.scss",
+    "@/assets/styles/theme.css",
+    "@/assets/styles/transition.css",
+    "@/assets/styles/markdown.scss",
   ],
   stylelint: {
     /* module options */
     lintOnStart: false,
   },
-})
+});
